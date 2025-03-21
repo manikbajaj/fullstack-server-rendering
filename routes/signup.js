bc6d759457ddfa35f95ce50f6d92144897f8bbfd
Manik <manik@pixelconception.com>
2025-03-21 14:03:13 +0530

    removing layout not needed

--- routes/signup.js
+++ routes/signup.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("signup", { layout: "layout" });
+  res.render("signup");
 });
 
 module.exports = router;
