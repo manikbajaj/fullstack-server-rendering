941dc607315504f85083d8dde2289d0be26ccbba
Manik <manik@pixelconception.com>
2025-03-22 17:09:07 +0530

    Final code for FSDB984

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,1 +1,6 @@
 import Quill from "quill";
+
+// Initialize Quill on a specific div with an ID of 'editor'
+const editor = new Quill("#editor", {
+  theme: "snow", // Set the default theme to 'snow'
+});
