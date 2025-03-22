2a86f35c594e937e7ccc81af45761482f445ed2b
Manik <manik@pixelconception.com>
2025-03-22 12:43:16 +0530

    Final code for FSDB971

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express " });
 });
 
+/* GET home page. */
+router.get("/create-post", function (req, res, next) {
+  console.log(req.params);
+  res.render("createPost");
+});
+
 module.exports = router;
