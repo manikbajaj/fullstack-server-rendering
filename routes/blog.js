813e951698e72cbb6ecdbef8d407964b15c68139
Manik <manik@pixelconception.com>
2025-03-27 14:54:31 +0530

    Final code for FSDB1045

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var router = express.Router();
+var { validationResult } = require("express-validator");
+var createPostValidator = require("../validators/createPost.validator.js");
 
 /* GET home page. */
 router.get("/:blogId", function (req, res, next) {
   console.log(req.params);
+  res.render("blog");
+});
+
+router.post("/create", createPostValidator, async function (req, res, next) {
+  const result = validationResult(req);
+  console.log(req.body);
+  console.log(result.array());
   res.render("blog");
 });
 
---
+++ validators/createPost.validator.js
@@ -0,0 +1,53 @@
+var { checkSchema } = require("express-validator");
+
+const createPostValidator = checkSchema({
+  title: {
+    in: ["body"],
+    notEmpty: true,
+    errorMessage: "Title is required.",
+    isString: true,
+    trim: true,
+  },
+  content: {
+    in: ["body"],
+    notEmpty: true,
+    errorMessage: "Content cannot be empty.",
+    isString: true,
+    trim: true,
+  },
+  slug: {
+    in: ["body"],
+    notEmpty: true,
+    errorMessage: "Slug is required and must be unique.",
+    isSlug: {
+      errorMessage: "Slug must be a valid URL slug.",
+    },
+    trim: true,
+  },
+  excerpt: {
+    in: ["body"],
+    notEmpty: true,
+    errorMessage: "Excerpt is required.",
+    isString: true,
+    trim: true,
+    isLength: {
+      options: { max: 255 },
+      errorMessage: "Excerpt cannot be more than 255 characters long.",
+    },
+  },
+  featuredImageUrl: {
+    in: ["body"],
+    notEmpty: true,
+    errorMessage: "Featured Image URL is required.",
+    isURL: {
+      options: {
+        protocols: ["http", "https"],
+        require_protocol: true,
+      },
+      errorMessage: "Must be a valid URL.",
+    },
+    trim: true,
+  },
+});
+
+module.exports = createPostValidator;
