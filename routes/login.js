409dd514735185e6ea0d13c177e0b76933152227
Manik <manik@pixelconception.com>
2025-03-27 11:48:50 +0530

    Final code for FSDB1034

--- routes/login.js
+++ routes/login.js
@@ -4,19 +4,6 @@ var prisma = require("../prisma/prismaClient.js");
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  try {
-    const user = await prisma.user.create({
-      data: {
-        firstName: "John", // Required field
-        email: "john@example.com", // Required and must be unique
-      },
-    });
-  } catch (error) {
-    console.error("Error creating user:", error);
-    // Handle specific errors like unique constraint violation etc.
-    throw error;
-  }
-
   res.render("login");
 });
 
