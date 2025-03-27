089e8f7fc372197bf0bf842cd81e6c162a889673
Manik <manik@pixelconception.com>
2025-03-27 13:58:40 +0530

    Final code for FSDB1041

---
+++ validators/signup.validators.js
@@ -0,0 +1,42 @@
+var { checkSchema } = require("express-validator");
+
+export const signupValidator = checkSchema({
+  firstName: {
+    in: ["body"],
+    notEmpty: true,
+    isString: true,
+    trim: true,
+  },
+  lastName: {
+    in: ["body"],
+    notEmpty: false,
+    isString: true,
+    trim: true,
+  },
+  email: {
+    in: ["body"],
+    notEmpty: true,
+    /* all options can be checked in validators.d.ts file in chain directory of express validator package */
+    isEmail: {
+      options: {
+        allow_underscores: true,
+      },
+    },
+    trim: true,
+  },
+  password: {
+    in: ["body"],
+    notEmpty: true,
+    isStrongPassword: {
+      options: {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      },
+      errorMessage:
+        "The password must be of 8 chars, with atleas 1 uppercase and 1 lowercase char, 1 number and 1 symbol.",
+    },
+  },
+});
