8a193d735274038e6fd5fdf1c5fa3d55e726ed1e
Manik <manik@pixelconception.com>
2025-03-30 13:40:36 +0530

    Final code for FSDB1055

--- controllers/signup/providers/createUser.provider.js
+++ controllers/signup/providers/createUser.provider.js
@@ -1,8 +1,17 @@
 const { matchedData } = require("express-validator");
+const bcrypt = require("bcrypt");
+const prisma = require("../../../prisma/prismaClient.js");
 
 async function createUserProvider(req, res) {
   const validatedData = matchedData(req);
-  console.log(validatedData);
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(validatedData.password, salt);
+
+  const newUser = await prisma.user.create({
+    data: { ...validatedData, password: hashedPassword },
+  });
+
+  console.log(newUser);
 }
 
 module.exports = { createUserProvider };
