4b57ae84ccd601c204900c2a8315682e59c3f752
Manik <manik@pixelconception.com>
2025-03-31 11:14:08 +0530

    Final code for FSDB1067

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,19 @@ router.get("/create-post", requireAuth, function (req, res, next) {
   res.render("createPost");
 });
 
+/* GET home page. */
+router.get("/signout", requireAuth, function (req, res, next) {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.send("Unable to signout please contact admin");
+    }
+
+    // Optionally clear the client-side cookie if it's set
+    res.clearCookie("connect.sid"); // 'connect.sid' is the default session cookie name, change if different
+
+    // Redirect to homepage or login page after logout
+    res.redirect("/");
+  });
+});
+
 module.exports = router;
