389f866a0aa60ea532d1735673b766b4d18c948d
Manik <manik@pixelconception.com>
2025-03-31 13:57:13 +0530

    Final code for FSDB1072

---
+++ prisma/seeds/users.seed.js
@@ -0,0 +1,26 @@
+// prisma/seeds/userSeed.js
+const prisma = require("../prismaClient.js");
+const faker = require("faker");
+const bcrypt = require("bcrypt");
+
+async function seedUsers() {
+  const numberOfUsers = 5;
+  const password = "TestPass123#";
+
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  for (let i = 0; i < numberOfUsers; i++) {
+    await prisma.user.create({
+      data: {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        password: hashedPassword, //faker.internet.password()
+      },
+    });
+  }
+  console.log(`${numberOfUsers} users created.`);
+}
+
+module.exports = seedUsers;
