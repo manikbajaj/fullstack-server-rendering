2e667f7a9aeddc3745c355f7ace7dd59913bb375
Manik <manik@pixelconception.com>
2025-04-01 11:20:59 +0530

    Final code for FSDB1082

--- controllers/blog/providers/getAllPosts.provider.js
+++ controllers/blog/providers/getAllPosts.provider.js
@@ -1,9 +1,48 @@
 const { matchedData } = require("express-validator");
+const prisma = require("../../../prisma/prismaClient.js");
+const util = require("util");
 
 async function getAllPostsProvider(req, res) {
   const validatedData = matchedData(req);
-  console.log(validatedData);
-  return res.render("index");
+  const limit = validatedData.limit ?? 10;
+  const page = validatedData.page ?? 1;
+  const tag = validatedData.tag;
+
+  const posts = await prisma.post.findMany({
+    skip: page * limit,
+    take: limit,
+    where: {
+      tags: {
+        some: {
+          slug: tag,
+        },
+      },
+    },
+    include: {
+      author: true, // includes details about the author
+      tags: true, // includes all tags related to each post
+    },
+  });
+
+  const totalPosts = await prisma.post.count({
+    where: {
+      tags: {
+        some: {
+          slug: tag,
+        },
+      },
+    },
+  });
+
+  const totalPages = Math.ceil(totalPosts / limit);
+
+  return res.render("index", {
+    posts,
+    currentPage: page,
+    limit,
+    totalPages,
+    tag,
+  });
 }
 
 module.exports = getAllPostsProvider;
