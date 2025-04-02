9593ae0a0b90dd6a769a91558577a715960674f9
Manik <manik@pixelconception.com>
2025-04-02 14:17:16 +0530

    Adding comments for lectures

--- routes/validations.js
+++ routes/validations.js
@@ -3,11 +3,13 @@ var router = express.Router();
 
 router.post("/email", function (req, res, next) {
   console.log(req.body);
+  /* This HTML will be added inside the input tag */
   res.send(`<p>changed</p>`);
 });
 
 router.post("/password", function (req, res, next) {
   console.log(req.body);
+  /* This HTML will be added inside the input tag */
   res.send(`<p>changed</p>`);
 });
 
