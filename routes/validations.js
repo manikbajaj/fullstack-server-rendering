89441a790ffcbef7177cd3475496a9a7fefa3099
Manik <manik@pixelconception.com>
2025-04-02 17:30:49 +0530

    Final code for FSDB1106

--- routes/validations.js
+++ routes/validations.js
@@ -1,19 +1,29 @@
 var express = require("express");
 var router = express.Router();
-var { validationResult } = require("express-validator");
+var { validationResult, matchedData } = require("express-validator");
 var emailValidator = require("../validators/formValidators/email.validator.js");
 
 router.post("/email", emailValidator, function (req, res, next) {
   const result = validationResult(req);
+  const validatedData = matchedData(req);
 
-  console.log(result.array());
+  console.log(req.body.email);
 
-  /* This HTML will be added inside the input tag */
-  res.render(`validations/testValidation`, { layout: false });
+  if (!result.isEmpty()) {
+    res.render(`validations/emailInValid`, {
+      layout: false,
+      value: req.body.email,
+    });
+    return;
+  }
+
+  res.render(`validations/emailValid`, {
+    layout: false,
+    value: validatedData.email,
+  });
 });
 
 router.post("/password", function (req, res, next) {
-  console.log(req.body);
   /* This HTML will be added inside the input tag */
   res.render(`validations/testValidation`, { layout: false });
 });
